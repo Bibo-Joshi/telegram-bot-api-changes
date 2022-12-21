3e92b708d9daa70d34c4ab2dac18c03bbb4cd89f
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2022-12-22 00:10:50 +0100

    Add Functionality

---
+++ README.md
@@ -0,0 +1,12 @@
+# Telegram Bot API Changes
+
+Yet another channel tool that keeps track of changes in the Telegram documentation related to the Bot API.
+
+This tool keeps track of two sets of web pages:
+
+* Technical reference pages (list of available [here](https://github.com/Bibo-Joshi/telegram-bot-api-changes/blob/master/reference_links.txt))
+* Supplementary documentation (list available [here](https://github.com/Bibo-Joshi/telegram-bot-api-changes/blob/master/reference_links.txt))
+
+Changes are posted to [this Telegram Channel](https://t.me/bot_api_changes).
+
+Heavily inspired by [this repository](https://github.com/spontanurlaub/telegram-bot-api-log) and [this channel](https://t.me/joinchat/AAAAAEVAbcr5AkmFd_dAIw).
---
+++ pull_changes.py
@@ -0,0 +1,137 @@
+import asyncio
+import datetime
+import platform
+import re
+import subprocess
+from argparse import ArgumentParser
+from collections.abc import Collection
+from enum import StrEnum
+from http import HTTPStatus
+
+from httpx import AsyncClient
+from telegram import Bot
+from telegram.constants import ParseMode
+
+class ChangeList(StrEnum):
+    SUPPLEMENTARY = "supplementary"
+    REFERENCE = "reference"
+
+
+PAGE_GENERATION_TIME_REGEX = re.compile(r"<!-- page generated in \d+\.\d+m?s -->")
+
+
+def get_urls(change_list: ChangeList) -> list[str]:
+    with open(f"{change_list.value}_links.txt", "r", encoding="utf-8") as f:
+        return [url.strip() for url in f.readlines()]
+
+
+def get_file_name(url: str) -> str:
+    return f'{url.replace("https://", "").replace("/", ".").replace(".telegram.org", "")}.html'
+
+
+def remove_page_generation_time(html: str) -> str:
+    return re.sub(PAGE_GENERATION_TIME_REGEX, "", html)
+
+
+async def download_one(url: str, client: AsyncClient) -> None:
+    response = await client.get(url)
+    if response.status_code != HTTPStatus.OK:
+        raise RuntimeError(f"Failed to download {url}: {response.status_code}")
+    with open(f"{get_file_name(url)}", "w", encoding="utf-8") as f:
+        f.write(remove_page_generation_time(response.text))
+
+
+async def download_all(urls: Collection[str]) -> None:
+    async with AsyncClient(timeout=20) as client:
+        await asyncio.gather(*(download_one(url, client) for url in urls))
+
+
+def diff_2_html(title: str) -> bytes | None:
+    full_title = (
+        f"Changes in {title} on {datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M')} UTC"
+    )
+    try:
+        completed_process = subprocess.run(
+            [
+                "diff2html",
+                "--style",
+                "side",
+                "--fileContentToggle",
+                "--output",
+                "stdout",
+                "--title",
+                full_title,
+            ],
+            check=True,
+            capture_output=True,
+            shell=platform.system() == "Windows",
+        )
+
+        return completed_process.stdout
+    except subprocess.CalledProcessError as exc:
+        if exc.returncode == 3:
+            return None
+        else:
+            raise exc
+
+
+async def send_to_telegram(
+    token: str, chat_id: int | str, html_data: bytes, filename: str, caption: str
+) -> None:
+    async with Bot(token) as bot:
+        await bot.send_document(
+            chat_id=chat_id,
+            document=html_data,
+            filename=filename,
+            caption=caption,
+            parse_mode=ParseMode.HTML,
+            write_timeout=60,
+            connect_timeout=60,
+            read_timeout=60
+        )
+
+
+def main(token: str, change_list: ChangeList) -> None:
+    if change_list == ChangeList.SUPPLEMENTARY:
+        title = "Supplementary Docs"
+        filename = "supplementary_docs.html"
+    else:
+        title = "Reference Docs"
+        filename = "reference_docs.html"
+
+    urls = get_urls(change_list)
+    caption_base = f"Changed <i>{title}</i>:\n\n"
+
+    asyncio.run(download_all(urls=urls))
+
+    if html_data := diff_2_html(title=title):
+        html_text = html_data.decode("utf-8")
+        caption_urls = "\n".join(
+                f"• <a href='{url}'>{url_file_name}</a>"
+                for url in urls
+                if (url_file_name := get_file_name(url)) in html_text
+        )
+        asyncio.run(
+            send_to_telegram(
+                token=token,
+                chat_id="@bot_api_changes",
+                html_data=html_data,
+                filename=filename,
+                caption=f"{caption_base}{caption_urls}",
+            )
+        )
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-T", "--token", type=str, required=True)
+    parser.add_argument(
+        "-L",
+        "--change-list",
+        type=str,
+        required=True,
+        choices=tuple(ChangeList),
+    )
+
+    arguments = parser.parse_args()
+    main(token=arguments.token, change_list=ChangeList(arguments.change_list))
